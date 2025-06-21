eb0a532ca1648b2e6291e3d6117537af83bd62f3
ilya <sid2009.is@gmail.com>
2025-06-21 19:59:38 +0300

    LVLSystem

--- DoSomething/Player.cs
+++ DoSomething/Player.cs
@@ -11,6 +11,9 @@ namespace DoSomething
         private int HP;
         private int ATTACK;
         private string CLASS;
+        private int LVL = 1;
+        private int XP = 0;
+        private double XPR = 10;
 
         public Player(string CLASS)
         {
@@ -39,9 +42,18 @@ namespace DoSomething
         public void SetCLASS(string CLASS) { this.CLASS = CLASS; }
         public string GetCLASS() { return this.CLASS; }
 
+        public void SetLVL(int LVL) { this.LVL = LVL; }
+        public int GETLVL() { return this.LVL; }
+
+        public void SetXP(int XP) { this.XP = XP; }
+        public int GETXP() { return this.XP; }
+
+        public void SetXPR(double XPR) { this.XPR = XPR; }
+        public double GETXPR() { return this.XPR; }
+
         public void ShowStats()
         {
-            Console.WriteLine($"HP: {this.HP} ATTACK: {this.ATTACK} CLASS: {this.CLASS}");
+            Console.WriteLine($"HP: {this.HP} ATTACK: {this.ATTACK} CLASS: {this.CLASS} LVL: {this.LVL} XP: {this.XP} XPR: {this.XPR}");
         }
     }
 }
