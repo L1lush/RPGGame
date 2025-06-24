da5f53cc5b0022a97f4f0fe146d6de103b87ec9c
ilya <sid2009.is@gmail.com>
2025-06-24 17:29:44 +0300

    Class Weapon

---
+++ DoSomething/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoSomething
+{
+    internal class Weapon
+    {
+        private string name;
+        private int ATTACK;
+
+        public Weapon(string name)
+        {
+            this.name = name;
+            switch (name)
+            {
+                case "Knife":
+                    this.ATTACK = 10;
+                    break;
+                case "Sword":
+                    this.ATTACK = 20;
+                    break;
+                case "Big Sword":
+                    this.ATTACK = 30;
+                    break;
+            }
+        }
+
+        public int GetATTACK()
+        {
+            return this.ATTACK;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public int NumOfWeapons()
+        {
+            return 3;
+        }
+    }
+}
