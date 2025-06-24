51b82fc73ce114b058cd225780f9719de9ca8a63
oravidor <110723602+oravidor@users.noreply.github.com>
2025-06-24 13:10:13 +0300

    simpler and add maxHp

--- DoSomething/Player.cs
+++ DoSomething/Player.cs
@@ -10,12 +10,14 @@ namespace DoSomething
     internal class Player
     {
         public int HP { get; set; }
+        public int MaxHP { get; set; } = 30;
         public int ATTACK { get; set; }
         public string CLASS { get; set; }
         public int LVL { get; set; } = 1;
         public int XP { get; set; } = 0;
-        public double XPR { get; set; } = 10;
+        public int XPR { get; set; } = 10;
         public int Gold { get; set; } = 0;
+        public int Positions { get; set; } = 5;
 
         public Player() { }
         public Player(string CLASS)
@@ -29,7 +31,7 @@ namespace DoSomething
                     this.ATTACK = 5;
                     break;
                 case "Assassin":
-                     this.HP = 15;
+                    this.HP = 15;
                     this.ATTACK = 7;
                     break;
             }
@@ -38,9 +40,29 @@ namespace DoSomething
 
         public void SetHP(int HP) { this.HP = HP; }
         public int GetHP() { return this.HP; }
+        public void AddHP(int amount)
+        {
+            this.HP += amount;
+            if (this.HP > this.MaxHP)
+            {
+                this.HP = this.MaxHP; // Ensure HP doesn't exceed MaxHP
+            }
+        }
+        public void SubtractHP(int amount)
+        {
+            this.HP -= amount;
+            if (this.HP < 0)
+            {
+                this.HP = 0; // Ensure HP doesn't go negative
+            }
+        }
 
         public void SetATTACK(int ATTACK) { this.ATTACK = ATTACK; }
-        public int GetATTACK() {  return this.ATTACK; }
+        public int GetATTACK() { return this.ATTACK; }
+        public void AddATTACK(int amount)
+        {
+            this.ATTACK += amount;
+        }
 
         public void SetCLASS(string CLASS) { this.CLASS = CLASS; }
         public string GetCLASS() { return this.CLASS; }
@@ -51,22 +73,56 @@ namespace DoSomething
         public void SetXP(int XP) { this.XP = XP; }
         public int GETXP() { return this.XP; }
 
-        public void SetXPR(double XPR) { this.XPR = XPR; }
-        public double GETXPR() { return this.XPR; }
+        public void SetXPR(int XPR) { this.XPR = XPR; }
+        public int GETXPR() { return this.XPR; }
 
-        public void SetGold(int Gold) {  this.Gold = Gold; }
-        public int GetGold() {  return this.Gold; }
+        public void SetGold(int Gold) { this.Gold = Gold; }
+        public int GetGold() { return this.Gold; }
+        public void AddGold(int amount)
+        {
+            this.Gold += amount;
+        }
+        public void SubtractGold(int amount)
+        {
+            this.Gold -= amount;
+            if (this.Gold < 0)
+            {
+                this.Gold = 0; // Ensure gold doesn't go negative
+            }
+        }
+
+        public void SetPositions(int Positions) { this.Positions = Positions; }
+        public int GetPositions() { return this.Positions; }
+        public void AddPositions(int amount)
+        {
+            this.Positions += amount;
+        }
+        public void SubtractPositions(int amount)
+        {
+            this.Positions -= amount;
+            if (this.Positions < 0)
+            {
+                this.Positions = 0; // Ensure positions don't go negative
+            }
+        }
+
+        public void SetMaxHP(int MaxHP) { this.MaxHP = MaxHP; }
+        public int GetMaxHP() { return this.MaxHP; }
+        public void AddMaxHP(int amount)
+        {
+            this.MaxHP += amount;
+        }
 
         public void ShowStats()
         {
-            Console.WriteLine($"HP: {this.HP} ATTACK: {this.ATTACK} CLASS: {this.CLASS} LVL: {this.LVL} XP: {this.XP} XPR: {this.XPR}  GOLD: {this.Gold}");
+            Console.WriteLine($"HP: {this.HP}  Max Hp: {this.MaxHP} ATTACK: {this.ATTACK} CLASS: {this.CLASS} LVL: {this.LVL} XP: {this.XP} XPR: {this.XPR}  GOLD: {this.Gold}  Positions: {this.Positions}");
         }
 
         public void LevelUp(int XPGOT)
         {
             int currentLVL = this.GETLVL();
             int currentXP = this.GETXP();
-            double requiredXP = this.GETXPR();
+            int requiredXP = this.GETXPR();
 
             int totalXP = currentXP + XPGOT;
 
@@ -74,7 +130,10 @@ namespace DoSomething
             {
                 totalXP -= (int)requiredXP;
                 currentLVL += 1;
-                requiredXP *= 1.25;
+                requiredXP = (int)(requiredXP * 1.25);
+                this.AddMaxHP(5); // Increase MaxHP on level up
+                this.AddHP(5); // Heal on level up
+                this.AddATTACK(2); // Increase ATTACK on level up
             }
 
             this.SetLVL(currentLVL);
