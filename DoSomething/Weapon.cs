3e0977650ca270b87f8985a8eafdf813b0ed95b9
oravidor <110723602+oravidor@users.noreply.github.com>
2025-06-24 18:51:56 +0300

    Refactor weapon system and enhance shop functionality
    Replaced Player's weapon array with a single Weapon object and updated related logic. The shop now offers a rotating selection of random weapons with unique stats and prices, and tracks equipped weapons. Weapon class expanded to support multiple weapon types, attack values, and prices. Improved shop UI and purchase logic to reflect these changes.

--- DoSomething/Weapon.cs
+++ DoSomething/Weapon.cs
@@ -11,22 +11,90 @@ namespace DoSomething
     {
         private string name;
         private int ATTACK;
+        private int price;
+
+        public Weapon()
+        {
+        }
 
         public Weapon(string name)
         {
             this.name = name;
             switch (name)
             {
-                case "Knife":
+                case "Sword":
+                    this.name = "Sword";
                     this.ATTACK = 10;
                     break;
-                case "Sword":
-                    this.ATTACK = 20;
-                    break;
                 case "Big Sword":
+                    this.name = "Big Sword";
                     this.ATTACK = 30;
                     break;
+                case "Axe":
+                    this.name = "Axe";
+                    this.ATTACK = 25;
+                    break;
+                case "Bow":
+                    this.name = "Bow";
+                    this.ATTACK = 15;
+                    break;
+                case "Spear":
+                    this.name = "Spear";
+                    this.ATTACK = 18;
+                    break;
+                case "Hammer":
+                    this.name = "Hammer";
+                    this.ATTACK = 28;
+                    break;
+                case "Dagger":
+                    this.name = "Dagger";
+                    this.ATTACK = 5;
+                    break;
             }
+        }
+
+        public void SetType(string name)
+        {
+            this.name = name;
+            switch (name)
+            {
+                case "Sword":
+                    this.name = "Sword";
+                    this.ATTACK = 10;
+                    this.price = 30;
+                    break;
+                case "Big Sword":
+                    this.name = "Big Sword";
+                    this.ATTACK = 30;
+                    this.price = 90;
+                    break;
+                case "Axe":
+                    this.name = "Axe";
+                    this.ATTACK = 25;
+                    this.price = 75;
+                    break;
+                case "Bow":
+                    this.name = "Bow";
+                    this.ATTACK = 15;
+                    this.price = 45;
+                    break;
+                case "Spear":
+                    this.name = "Spear";
+                    this.ATTACK = 18;
+                    this.price = 55;
+                    break;
+                case "Hammer":
+                    this.name = "Hammer";
+                    this.ATTACK = 28;
+                    this.price = 85;
+                    break;
+                case "Dagger":
+                    this.name = "Dagger";
+                    this.ATTACK = 8;
+                    this.price = 24;
+                    break;
+            }
+            
         }
 
         public int GetATTACK()
@@ -34,13 +102,19 @@ namespace DoSomething
             return this.ATTACK;
         }
 
+        public int GetPrice()
+        {
+            return this.price;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
         public void SetName(string name)
         {
             this.name = name;
-        }
-        public int NumOfWeapons()
-        {
-            return 3;
         }
     }
 }
